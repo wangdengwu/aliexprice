7f82c25659003f095c76a60c26339bd5aefa592f
wangdengwu <dengwu.wang@gmail.com>
2015-05-28 14:44:49 +0800

    add coupon page

--- application/controllers/coupon.php
+++ application/controllers/coupon.php
@@ -10,7 +10,40 @@ class Coupon extends MpController
 
     public function doIndex()
     {
-        $viewData=array('productName'=>'Coupon');
+        $html = file_get_html('http://coupon.aliexpress.com/');
+        $lis = array();
+        $urls = array();
+        foreach($html->find('.coupon-list li') as $element){
+            $link = $element->children(0);
+            $href=$link->href;
+            $img=$link->children(0)->children(0)->src;
+            $store=$link->children(1)->children(0)->innertext;
+            $price=$link->children(1)->children(1)->children(0)->innertext;
+            $order=$link->children(1)->children(2)->children(0)->innertext;
+            $time=$link->children(1)->children(3)->innertext;
+            $li = (object) array('href' => $href, 'img' => $img, 'store' => $store, 'price' => $price, 'order' => $order, 'time' => $time);
+            array_push($lis,$li);
+            array_push($urls,$href);
+        }
+        $api_url="http://gw.api.alibaba.com/openapi/param2/2/portals.open/api.getPromotionLinks/85087";
+        $http = new WoniuHttp();
+        $data['trackingId'] = "aliexprice";
+        $data['fields'] = "url,promotionUrl";
+        $data['urls'] = join(",",$urls);
+        $ref="http://www.aliexprice.com/";
+        $result=$http->setReferer($ref)->post($api_url, $data);
+//        echo $result;
+        $result_object=json_decode($result);
+        $hrefs = $result_object->result->promotionUrls;
+        for($i=0;$i<count($hrefs);$i++){
+            $url = $hrefs[$i];
+            $item = $lis[$i];
+            $item->href = $url->promotionUrl;
+        }
+//        var_dump($lis);
+        $viewData=array('items'=>$lis,'title'=>"Aliexprice.com - Online Shopping for Electronics, Fashion, Home &amp; Garden, Toys &amp; Sports, Automobiles from China.",
+                        'keywords'=>"Aliexprice, Online shopping, Shopping Online, Online Marketing, Automotive, Phones, Accessories, Computers, Electronics, Fashion, Beauty, Health, Home, Garden, Toys , Sports, Weddings, Events",
+                        'description'=>"Online Shopping for Cheap Automotive, Phones Accessories, Computers Electronics, Fashion, Beauty Health, Home Garden, Toys Sports, Weddings Events from China; Shopping on Aliexprice |the world's Online Marketing place.");
         $this->view('coupon',$viewData);
     }
 
--- application/views/coupon.view.php
+++ application/views/coupon.view.php
@@ -1,10 +1,89 @@
-<?php include($this->view_path('common/header'));?>
-<title><?php echo $productName.' and more on Aliexprice.com' ?></title>
-<meta name="keywords" content="<?php echo $productName.',Online Shopping '.$productName ?>" />
-<meta name="description" content="<?php echo $productName.' and more from Aliexpress.com, Factory Price, Worldwide Shipping!' ?>" />
+<?php include($this->view_path('common/header')); ?>
+<title><?php echo $title ?></title>
+<meta name="keywords" content="<?php echo $keywords ?>" />
+<meta name="description" content="<?php echo $description ?>" />
 </head>
 <body>
 
-<h1>Coupon Detail</h1>
-
-<?php include($this->view_path('common/footer'));?>
+<div class="container-fluid">
+    <div class="container">
+	    <div class="top-banner">
+            <img class="center-block" src="http://atms02.alicdn.com/tps/i2/TB1ecKwHVXXXXXKXVXXsvvJHXXX-990-280.jpg">
+	    </div>
+    </div>
+    <div class="mod-title min-width center-block">
+		<h3>Seller Coupons</h3>
+	</div>
+	<div class="container">
+		<div class="center-block coupon-body">
+			<div class="col-main">
+				<div class="main-wrap">
+					<div class="mod-main">
+                        <ul class="coupon-list clearfix">
+                        <?php
+                            for($i=0;$i<count($items);$i++){
+                                $item = $items[$i];
+                                $href=$item->href;
+                                $img=$item->img;
+                                $store=$item->store;
+                                $price=$item->price;
+                                $order=$item->order;
+                                $time=$item->time;
+                         ?>
+                            <li>
+								<a rel="nofollow" href="<?php echo $href ?>">
+									<div class="store-img">
+                                        <img src="<?php echo $img ?>">
+                                    </div>
+									<div class="coupon-info">
+                                        <span class="store-name"><?php echo $store ?></span>
+                                        <span class="coupon-price"><em><?php echo $price ?></em> off</span>
+										<span class="coupon-order-price">per <em><?php echo $order ?></em></span>
+										<span class="coupon-time"><?php echo $time ?></span>
+									</div>
+								</a>
+							</li>
+                         <?php } ?>
+                        </ul>
+                    </div>
+				</div>
+			</div>
+            <div class="col-sub">
+                <div class="categories-list">
+                    <ul>
+                        <li><a href="/catlist.index/3" >Apparel & Accessories</a></li>
+                        <li><a href="/catlist.index/34" >Automobiles & Motorcycles</a></li>
+                        <li><a href="/catlist.index/1501" >Baby Products</a></li>
+                        <li><a href="/catlist.index/66" >Beauty & Health</a></li>
+                        <li><a href="/catlist.index/7" >Computer & Networking</a></li>
+                        <li><a href="/catlist.index/13" >Construction & Real Estate</a></li>
+                        <li><a href="/catlist.index/44" >Consumer Electronics</a></li>
+                        <li><a href="/catlist.index/100008578" >Customized Products</a></li>
+                        <li><a href="/catlist.index/5" >Electrical Equipment & Supplies</a></li>
+                        <li><a href="/catlist.index/502" >Electronic Components & Supplies</a></li>
+                        <li><a href="/catlist.index/2" >Food</a></li>
+                        <li><a href="/catlist.index/1503" >Furniture</a></li>
+                        <li><a href="/catlist.index/200003655" >Hair & Accessories</a></li>
+                        <li><a href="/catlist.index/42" >Hardware</a></li>
+                        <li><a href="/catlist.index/15" >Home & Garden</a></li>
+                        <li><a href="/catlist.index/6" >Home Appliances</a></li>
+                        <li><a href="/catlist.index/200003590" >Industry & Business</a></li>
+                        <li><a href="/catlist.index/36" >Jewelry & Watch</a></li>
+                        <li><a href="/catlist.index/39" >Lights & Lighting</a></li>
+                        <li><a href="/catlist.index/1524" >Luggage & Bags</a></li>
+                        <li><a href="/catlist.index/21" >Office & School Supplies</a></li>
+                        <li><a href="/catlist.index/509" >Phones & Telecommunications</a></li>
+                        <li><a href="/catlist.index/30" >Security & Protection</a></li>
+                        <li><a href="/catlist.index/322" >Shoes</a></li>
+                        <li><a href="/catlist.index/200001075" >Special Category</a></li>
+                        <li><a href="/catlist.index/18" >Sports & Entertainment</a></li>
+                        <li><a href="/catlist.index/1420" >Tools</a></li>
+                        <li><a href="/catlist.index/26" >Toys & Hobbies</a></li>
+                        <li><a href="/catlist.index/1511" >Watches</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include($this->view_path('common/footer')); ?>
