c56d22a7298233bc47a2e392b84613ce522d6566
wangdengwu <dengwu.wang@gmail.com>
2015-06-12 08:49:55 +0800

    modify the 404 page

--- application/error/error_404.php
+++ application/error/error_404.php
@@ -1,11 +1,66 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>404</title>
-        <link href="/css/aliexprice.css" rel="stylesheet">
-    </head>
+<html lang="en">
+<head>
+    <meta http-equiv="content-language" content="en" />
+    <meta name="google-site-verification" content="guru5ryWPoLP3GEntM_VBjuvN5Mzp9hqJPQUtvF17Ec" />
+    <meta name="msvalidate.01" content="6545332476A9BB36AB3DF66DDB11E8F6" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <!--    <link href="css/bootstrap-theme.min.css" rel="stylesheet">-->
+    <link href="/css/aliexprice.css?v=3.1" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-63782659-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+    <title>404</title>
+    <meta name="keywords" content="404 not found" />
+    <meta name="description" content="page not found" />
+</head>
     <body>
-        <a id="homepage-link" href="http://www.aliexprice.com/">Go to the AliExpress homepage</a>
+    <nav class="navbar navbar-default" id="header">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/">
+                    <span><img alt="AliFastShopping" src="/img/logo-new-1x.png"></span>
+                    <span class="logo-slogan">Smarter Shopping, Better Living!</span>
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="/">Home</a></li>
+                    <li><a target="_blank" rel="nofollow" href="http://s.click.aliexpress.com/e/j6EuFYjAa">Buyer Protection</a></li>
+                    <li><a target="_blank" class="coupon-link" href="/coupon.index">Coupon</a></li>
+                </ul>
+                <form method="post" action="/catlist.index" class="navbar-form navbar-left" role="search">
+                    <div class="form-group">
+                        <input type="text" name="keywords" class="form-control" placeholder="I'm shopping for...">
+                    </div>
+                    <button type="submit" class="btn btn-default">GO!</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+        <a id="homepage-link" href="http://www.aliexprice.com/">Go to the AliExprice homepage</a>
+        <footer >
+            <p class="text-center">&copy; aliexprice.com 2015</p>
+        </footer>
+        <script src="/js/jquery-1.11.1.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
     </body>
 </html>
